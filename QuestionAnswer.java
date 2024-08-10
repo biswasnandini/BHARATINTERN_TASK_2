66d6bd9f386a5ae2abd963159da17a25a3a11824
Nandini Biswas <114243527+biswasnandini@users.noreply.github.com>
2024-08-10 23:18:14 +0530

    Add files via upload

---
+++ QuestionAnswer.java
@@ -0,0 +1,28 @@
+package com.example.quiz_app;
+
+public class QuestionAnswer {
+
+    public static String question []={
+            "What country has the highest life expectancy?",
+            "What character have both Robert Downey Jr. and Benedict Cumberbatch played?",
+            "How many elements are in the periodic table?",
+            "What company was originally called Cadabra? ",
+            "How many minutes are in a full week? "
+    };
+
+    public static String choices [][]={
+            {"India","Vietnam","Hong Kong","France"},
+            {"Charlie Chaplin","Sherlock Holmes","Iron Man","None of the above"},
+            {"180","110","118","108"},
+            {"Amazon","SpaceX","Google","None of the above"},
+            {"12,544","10,893","12,587","10,080"}
+    };
+
+    public static String correctAnswers []={
+            "Hong Kong",
+            "Sherlock Holmes",
+            "118",
+            "Amazon",
+            "10,080"
+    };
+}
